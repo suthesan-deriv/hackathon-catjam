81d8edf4dc89d11448560276eb9b39247d24e7f2
Suthesan <suthesan@Suthesans-MacBook-Pro.local>
2022-02-08 15:36:51 +0800

    integrated ui with api

--- pages/components/BarcodeComponent/index.js
+++ pages/components/BarcodeComponent/index.js
@@ -1,39 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import {
-  outerDiv
-} from './style'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { outerDiv } from './style'
 
-
-const BarcodeComponent = ({ gif = '' }) => {
-  const src = 'https://static.beaconstac.com/assets/img/qrcg-api-bnw-1.png';
+const BarcodeComponent = ({ qr_url, gif = '' }) => {
+  // const src = 'https://static.beaconstac.com/assets/img/qrcg-api-bnw-1.png'
 
   return (
     <div>
-      <div className={outerDiv}>
-        <Image
-          alt="barcode"
-          loader={() => src}
-          src={src}
-          unoptimized
-          width={300}
-          height={300}
-        />
+      {qr_url && (
+        <div className={outerDiv}>
+          <Image
+            alt="barcode"
+            loader={() => qr_url}
+            src={qr_url}
+            unoptimized
+            width={300}
+            height={300}
+          />
+        </div>
+      )}
 
-      </div>
       <div className={outerDiv}>
-        {gif && <Image
-          alt="barcode"
-          loader={() => gif}
-          unoptimized
-          src={gif}
-          width={300}
-          height={300}
-        />}
-
+        {gif && (
+          <Image alt="barcode" loader={() => gif} unoptimized src={gif} width={300} height={300} />
+        )}
       </div>
     </div>
   )
 }
 
-export default BarcodeComponent;
+export default BarcodeComponent
