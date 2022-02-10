3caff1b42c398e3ce572b50475dc32823f38c6d0
Rupato <rupato@deriv.com>
2022-02-10 22:44:34 +0800

    git changes component structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import { css } from '@emotion/css';
-import Loader  from './components/Loader';
+import Loader  from '../components/Loader';
 
 const main = css({
 	position: 'relative'
